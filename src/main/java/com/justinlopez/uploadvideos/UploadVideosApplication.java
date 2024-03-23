c34737d8e7c7baa5250493a889d4979797df1de1
Justin Lopez <justinlopez588@gmail.com>
2024-03-23 12:52:11 -0500

    First commit

---
+++ src/main/java/com/justinlopez/uploadvideos/UploadVideosApplication.java
@@ -0,0 +1,13 @@
+package com.justinlopez.uploadvideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadVideosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadVideosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/justinlopez/uploadvideos/UploadVideosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justinlopez.uploadvideos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadVideosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
